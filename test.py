72cf227a2b87d6f077a329b58606b1e06a6cd25e
Wison Liu <31965362+850078897@users.noreply.github.com>
2018-03-14 09:49:53 -0400

    x
    1

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ white = (255,255,255)
 clock = pygame.time.Clock()
 crashed = False
 carImg = pygame.image.load('space.jpg')
+
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
@@ -42,6 +43,56 @@ while not crashed:
     gameDisplay.fill(white)
 
     gameDisplay.blit(carImg, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
+
+
+
+
+
+
+
+
+
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+crashed = False
+carImg2 = pygame.image.load('space2.jpg')
+
+def car(x,y):
+    gameDisplay.blit(carImg2, (x,y))
+
+x =  (display_width * 0.45)
+y = (display_height * 0.8)
+x_change = 0
+car_speed = 0
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+            x += x_change
+
+    gameDisplay.fill(white)
+
+    gameDisplay.blit(carImg2, (0, 0))
     pygame.display.update()
     clock.tick(60)
 
