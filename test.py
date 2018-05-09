25311b6871ed05b69deac307291dd0d0d4032401
Wison Liu <31965362+850078897@users.noreply.github.com>
2018-05-09 15:02:27 -0400

    1
    1

--- test.py
+++ test.py
@@ -1,22 +1,80 @@
 import pygame
+import pygame_textinput
+import test_game
+from pygame.locals import *
+
+from pygame.compat import unichr_, unicode_
+
+import sys
+
+import locale
+
+
+from itertools import chain
+
+
+def truncline(text, font, maxwidth):
+    real = len(text)
+    stext = text
+    l = font.size(text)[0]
+    cut = 0
+    a = 0
+    done = 1
+    old = None
+    while l > maxwidth:
+        a = a + 1
+        n = text.rsplit(None, a)[0]
+        if stext == n:
+            cut += 1
+            stext = n[:-cut]
+        else:
+            stext = n
+        l = font.size(stext)[0]
+        real = len(stext)
+        done = 0
+    return real, done, stext
+
+
+def wrapline(text, font, maxwidth):
+    done = 0
+    wrapped = []
+
+    while not done:
+        nl, done, stext = truncline(text, font, maxwidth)
+        wrapped.append(stext.strip())
+        text = text[nl:]
+    return wrapped
+
+
+def wrap_multi_line(text, font, maxwidth):
+    """ returns text taking new lines into account.
+    """
+    lines = chain(*(wrapline(line, font, maxwidth) for line in text.splitlines()))
+    return list(lines)
 
 pygame.init()
 
 display_width = 800
 display_height = 533
 
+
+clock = pygame.time.Clock()
+crashed= False
+intro_page= pygame.image.load('space.jpg')
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('lubenwei')
+
+textinput = pygame_textinput.TextInput()
+
+fg = 250,240,230
+bg = 5,5,5
+font = pygame.font.Font(None, 80)
+text = 'Fonty'
+size = font.size(text)
+
 
 black = (0,0,0)
 white = (255,255,255)
-
-clock = pygame.time.Clock()
-crashed = False
-carImg = pygame.image.load('space.jpg')
-
-def car(x,y):
-    gameDisplay.blit(carImg, (x,y))
 
 x =  (display_width * 0.45)
 y = (display_height * 0.8)
@@ -24,6 +82,7 @@ x_change = 0
 car_speed = 0
 
 while not crashed:
+    events = pygame.event.get()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
@@ -42,59 +101,39 @@ while not crashed:
 
     gameDisplay.fill(white)
 
-    gameDisplay.blit(carImg, (0, 0))
+    gameDisplay.blit(intro_page, (0, 0))
+
+    # load font, prepare values
+
+    font = pygame.font.Font(None, 80)
+
+    text = test_game.newbee
+
+    textlist = wrapline(text,font,20)
+
+    size = font.size(text)
+
+    # no AA, no transparancy, normal
+
+    ren = font.render(text, 0, fg, bg)
+
+    gameDisplay.blit(ren, (10, 10))
+
+    # Feed it with events every frame
+    textinput.update(events)
+    # Blit its surface onto the screen
+    gameDisplay.blit(textinput.get_surface(), (10, 10))
+
+    a_sys_font = pygame.font.SysFont("BaBa", 60)
+    a_sys_font.set_bold(1)
+    mytext = textinput.get_text()
+    ren = a_sys_font.render(mytext , 1 , fg, bg)
+    gameDisplay.blit(ren, (0,60))
+    a_sys_font.set_bold
+
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
 quit()
 
-
-
-
-
-
-
-
-
-
-black = (0,0,0)
-white = (255,255,255)
-
-clock = pygame.time.Clock()
-crashed = False
-carImg2 = pygame.image.load('space2.jpg')
-
-def car(x,y):
-    gameDisplay.blit(carImg2, (x,y))
-
-x =  (display_width * 0.45)
-y = (display_height * 0.8)
-x_change = 0
-car_speed = 0
-
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-
-            x += x_change
-
-    gameDisplay.fill(white)
-
-    gameDisplay.blit(carImg2, (0, 0))
-    pygame.display.update()
-    clock.tick(60)
-
-pygame.quit()
-quit()
--- textinput.py
+++ textinput.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import pygame_textinput
 import pygame
+import pygame_textinput
 pygame.init()
 
 # Create TextInput-object
-textinput = pygame_textinput.TextInput()
+input = pygame_textinput.TextInput()
 
 fg = 250,240,230
 bg = 5,5,5
@@ -23,13 +23,13 @@ while True:
             exit()
 
     # Feed it with events every frame
-    textinput.update(events)
+    input.update(events)
     # Blit its surface onto the screen
-    screen.blit(textinput.get_surface(), (10, 10))
+    screen.blit(input.get_surface(), (10, 10))
 
     a_sys_font = pygame.font.SysFont("BaBa", 60)
     a_sys_font.set_bold(1)
-    mytext = textinput.get_text()
+    mytext = input.get_text()
     ren = a_sys_font.render(mytext , 1 , fg, bg)
     screen.blit(ren, (0,60))
     a_sys_font.set_bold
